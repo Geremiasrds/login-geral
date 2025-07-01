ce4d5b34a216c0742a69db3af37401ddde4292a3
Geremias <geremiasds00@gmail.com>
2025-07-01 14:11:26 -0300

    correções de erros

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/login-geral/', // ⚠️ substitua pelo nome real do seu repositório
+  base: '/login-geral/',
 })
