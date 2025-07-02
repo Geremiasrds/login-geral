f94184de7d7277fa5265047e65a12b9593ed0801
Geremias <geremiasds00@gmail.com>
2025-07-01 23:08:46 -0300

    nova atualização de estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import { Routes, Route } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TelaInicial from "./componentes/TelaInicial";
 import Geral from "./componentes/orcamento/componentes/GeralOrcamento";
 import LoginGeral from "./TelaDeLogin";
 import ListaDeTarefas from "./componentes/lista-de-atividades/componentes/ListaDeAtividades";
 import GeralRecibo from "./componentes/recbibo/GeralRecibo";
 
-
-
 function App() {
   const [logado, setLogado] = useState(false);
 
+  useEffect(() => {
+    const estaLogado = localStorage.getItem("logado");
+    if (estaLogado === "true") {
+      setLogado(true);
+    }
+  }, []);
+
   const aoLogar = () => {
     setLogado(true);
+    localStorage.setItem("logado", "true");
   };
 
   return (
@@ -21,8 +27,8 @@ function App() {
         <Routes>
           <Route path="/" element={<TelaInicial />} />
           <Route path="/orcamento" element={<Geral />} />
-          <Route path="/tarefas" element={<ListaDeTarefas />}/>
-          <Route path="/recibo" element={<GeralRecibo />}/>
+          <Route path="/tarefas" element={<ListaDeTarefas />} />
+          <Route path="/recibo" element={<GeralRecibo />} />
         </Routes>
       ) : (
         <LoginGeral aoLogar={aoLogar} />
